85362f6197f5960d0a963362170a71933ccc0f36
Ian Waldron <ian.waldron@scribawaldron.com>
2025-06-03 08:20:03 -0700

    implement demo

--- demo/models.py
+++ demo/models.py
@@ -1,21 +1,39 @@
+import zoneinfo
+
 from django.db import models
 
 
+FRESHMAN = "FR"
+SOPHOMORE = "SO"
+JUNIOR = "JR"
+SENIOR = "SR"
+GRADUATE = "GR"
+YEAR_IN_SCHOOL_CHOICES = {
+    FRESHMAN: "Freshman",
+    SOPHOMORE: "Sophomore",
+    JUNIOR: "Junior",
+    SENIOR: "Senior",
+    GRADUATE: "Graduate",
+}
+
+YEAR_IN_SCHOOL_CHOICES_SET = sorted(set(YEAR_IN_SCHOOL_CHOICES.items()))
+
+
 class Student(models.Model):
-    FRESHMAN = "FR"
-    SOPHOMORE = "SO"
-    JUNIOR = "JR"
-    SENIOR = "SR"
-    GRADUATE = "GR"
-    YEAR_IN_SCHOOL_CHOICES = {
-        FRESHMAN: "Freshman",
-        SOPHOMORE: "Sophomore",
-        JUNIOR: "Junior",
-        SENIOR: "Senior",
-        GRADUATE: "Graduate",
-    }
+
     year_in_school = models.CharField(
         max_length=2,
-        choices=YEAR_IN_SCHOOL_CHOICES,
+        choices=YEAR_IN_SCHOOL_CHOICES_SET,
         default=FRESHMAN,
     )
+
+
+# time zones example
+
+timezones = sorted(zoneinfo.available_timezones())
+
+TIMEZONE_CHOICES = tuple(zip(timezones, timezones))
+
+
+class UserProfile(models.Model):
+    timezone = models.CharField(choices=TIMEZONE_CHOICES)
